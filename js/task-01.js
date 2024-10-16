3c5ebb073006d8faa0c8c997b732b9d3e3a74fa0
Vlad <vlad_np@ukr.net>
2024-10-16 16:45:07 +0200

    add start

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,109 @@
-const numberOfCategories = document.querySelectorAll(`#categories > li.item`)
-const nameOfCategories = document.querySelectorAll(`#categories > li.item > h2`)
-const numberCategoryEl =  document.querySelectorAll(`h2 ~ ul`)
 
-console.log(`Number of categories: ${numberOfCategories.length}`);
+let select = (s) => document.querySelector(s),
+  selectAll = (s) => document.querySelectorAll(s),
+  penguinTop = select(".penguinTop"),
+  penguinHead = select(".penguinHead"),
+  maxDragY = 260,
+  dragPosY = 0,
+  stretchBoxInitHeight = 7,
+  armL = select(".armL"),
+  armR = select(".armR"),
+  maxArmRotation = 100,
+  maxHeadRotation = 12;
 
-for(let i = 0; i < numberOfCategories.length; i += 1) {
-    console.log(`Category: ${nameOfCategories[i].textContent}`);
-    console.log(`Elements: ` + numberOfCategories[i].querySelectorAll(`ul > li`).length);
-    console.log(`Elements (by the children): ` + numberCategoryEl[i].children.length);
-}
+gsap.set("svg", {
+  visibility: "visible",
+});
 
-// const list = document.querySelector('#categories');
-// const items = list.querySelectorAll('.item');
-// console.log(`В списке ${items.length} категории:`);
-// items.forEach(node => {const h2 = node.querySelector('h2');const nodeItems = node.querySelectorAll('li');
-// console.log(` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`);}); 
+gsap.set(armL, {
+  transformOrigin: "100% 5%",
+});
+gsap.set(armR, {
+  transformOrigin: "30% 20%",
+});
+gsap.set(penguinHead, {
+  transformOrigin: "80% 95%",
+});
+
+let onDrag = () => {
+  dragPosY = gsap.getProperty(penguinTop, "y");
+  dragPosY = dragPosY <= 0 ? dragPosY : 0;
+  gsap.set(".stretch rect", {
+    attr: {
+      height: stretchBoxInitHeight - dragPosY,
+    },
+    y: dragPosY,
+    roundProps: "height,y",
+  });
+
+  gsap.to(armL, {
+    duration: 0.8,
+    rotation: (-dragPosY / maxDragY) * maxArmRotation,
+    ease: "elastic(0.65, 0.34)",
+  });
+  gsap.to(armR, {
+    duration: 1,
+    rotation: (dragPosY / maxDragY) * maxArmRotation,
+    ease: "elastic(0.5, 0.36)",
+  });
+  gsap.to(penguinHead, {
+    duration: 1,
+    rotation: (dragPosY / maxDragY) * maxHeadRotation,
+    ease: "elastic(0.85, 0.31)",
+  });
+};
+
+var topDrag = Draggable.create(penguinTop, {
+  type: "y",
+  bounds: { minY: -maxDragY, maxY: 0 },
+  onDrag: onDrag,
+  throwProps: true,
+  onThrowUpdate: onDrag,
+  overshootTolerance: 0,
+  onThrowComplete: () => {
+    var duration = -dragPosY / maxDragY;
+    gsap.delayedCall(duration / 2, resetPenguin);
+  },
+});
+
+let resetPenguin = () => {
+  var duration = -dragPosY / maxDragY;
+  gsap.to(penguinTop, {
+    duration: duration + 0.1,
+    y: 0,
+    onUpdate: onDrag,
+    ease: `elastic(${duration / 2}, 0.4)`, // Elastic.easeOut.config(duration/2 , 0.4)
+  });
+};
+
+let headNod = (_duration) => {
+  gsap.to(penguinHead, {
+    duration: 0.6,
+    rotation: -8,
+    delay: _duration / 2,
+    ease: "wiggle({type:uniform, wiggles:2})",
+  });
+};
+
+let blink = () => {
+  gsap.to(".eye", 0.3, {
+    opacity: 0,
+    delay: "random(1, 10)",
+    ease: "wiggle({type:uniform, wiggles:1})",
+    onComplete: blink,
+  });
+};
+gsap.defaults({ lazy: false });
+let init = () => {
+  blink();
+  gsap.to(penguinTop, {
+    duration: 0.6,
+    y: -160,
+    onUpdate: onDrag,
+    onComplete: () => {
+      gsap.delayedCall(0.3, resetPenguin);
+    },
+  });
+};
+
+init();
