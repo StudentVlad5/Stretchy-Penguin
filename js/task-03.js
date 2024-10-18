9160df6f8939e40cf7a797fc50b04e16553c2de6
Vlad <vlad_np@ukr.net>
2024-10-18 16:28:09 +0200

    add marry chr

--- js/task-03.js
+++ js/task-03.js
@@ -1,37 +1,110 @@
-const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+gsap.registerPlugin(Draggable, InertiaPlugin);
+
+// make snowflakes
+// lil snowflake generator
+class Snowflake {
+  constructor() {
+    this.el = document.createElementNS("http://www.w3.org/2000/svg", "image");
+    const href = `https://assets.codepen.io/16327/flake-${gsap.utils.random(1,6,1)}.png`;
+    const size = gsap.utils.random([60, 30, 70]);
+    const xPosition = gsap.utils.random(0, 1000);
+
+    this.el.setAttributeNS("http://www.w3.org/1999/xlink", "href", href);
+    this.el.setAttributeNS(null, "width", size);
+    this.el.setAttributeNS(null, "height", size);
+    this.el.setAttributeNS(null, "x", xPosition);
+
+    this.el.setAttributeNS(null, "y", -50);
+  }
+
+  get element() {
+    return this.el;
+  }
+}
+// create snowflakes
+function createSnowflakes() {
+  const snowContainer = document.getElementById("snow");
+
+  for (let i = 0; i < 30; i++) {
+    const snowflake = new Snowflake();
+    snowContainer.appendChild(snowflake.element);
+  }
+}
+createSnowflakes();
+
+let snowfall = gsap.to("#snow image", {
+  y: 1000,
+  ease: "none",
+  duration: "random([3, 4, 5])",
+  rotation: "random(-360, 360, 5)",
+  x: "random(-1000, 1000, 5)",
+  transformOrigin: "center",
+  stagger: {
+    amount: 5,
+    repeat: -1
   },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-];
-
-const listGalleryElem = document.querySelector('.gallery');
-listGalleryElem.style.display='flex';
-listGalleryElem.style.justifyContent='center';
-listGalleryElem.style.flexWrap='wrap';
-listGalleryElem.style.flexDirection='column';
-listGalleryElem.style.gap='10px';
+  paused: true
+});
 
 
+const coords = document.querySelector("#coords");
+const tracker = InertiaPlugin.track("#snowglobe", "x,y")[0];
 
-let arrayGalleryElem = [];
-for (let i = 0; i < images.length; i +=1) {
-  let liGalleryElem = document.createElement('li');
-  liGalleryElem.classList.add('img-gallery');
-  const imgEL = liGalleryElem.appendChild(document.createElement('img'));
-  imgEL.src = images[i].url;
-  imgEL.alt = images[i].alt;
-  imgEL.style.width = "200px";
-  arrayGalleryElem.push(liGalleryElem);
+gsap.set("#snowman", { transformOrigin: "bottom center" });
+
+const wobbleSnowman = gsap.quickTo("#snowman", "rotation", { duration: 0.4 });
+const moveSkyX = gsap.quickTo("#sky", "xPercent", { duration: 0.4 });
+
+function transformer(min, max, value) {
+  let clamp = gsap.utils.clamp(-2000, 2000);
+  let mapToRange = gsap.utils.mapRange(-2000, 2000, min, max);
+  let transform = gsap.utils.pipe(clamp, mapToRange);
+  return transform(value);
 }
 
-// arrayGalleryElem.forEach((key)=>listGalleryElem.appendChild(key));
-arrayGalleryElem.forEach((key)=>listGalleryElem.insertAdjacentHTML("afterBegin", key.innerHTML));
+let isSnowing = false;
+Draggable.create("#snowglobe", {
+  inertia: true,
+  bounds: "body",
+  onDragStart: () => {
+    isSnowing = true;
+
+    gsap.to("#snow", {
+      opacity: 1,
+      overwrite: true
+    });
+    snowfall.play(0);
+  },
+  onDragEnd: () => {
+    isSnowing = false;
+
+    gsap.to("#snow", {
+      opacity: 0,
+      overwrite: true,
+      onComplete: () => {
+        if (isSnowing) return;
+        console.log("pause");
+        snowfall.pause();
+      }
+    });
+  }
+});
+
+function updateOnVelocity() {
+  let x = tracker.get("x");
+
+  if (x === 0) return;
+  coords.innerHTML = x;
+
+  wobbleSnowman(transformer(90, -90, x));
+  moveSkyX(transformer(40, -40, x));
+
+  let speed = transformer(-10, 10, x);
+
+  gsap.to(snowfall, {
+    duration: 1,
+    timeScale: Math.abs(speed) + 0.2
+  });
+}
+
+gsap.ticker.add(updateOnVelocity);
